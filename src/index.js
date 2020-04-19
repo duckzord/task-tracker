12c4d9ab05fb40cd5807a62084ebac7087507295
Tom Gray <tom.gray@pitchtechnologies.com>
2020-04-19 13:20:57 +0100

    Initial test with React

---
+++ src/index.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Display some tasks
+
+// Add a new task
+
+class TaskContainer extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      userName: 'Tom Gray',
+      taskList: ['Do this', 'Do that', 'Also do this']
+    }
+
+    this.addTask = this.addTask.bind(this)
+  }
+  addTask(task) {
+    this.setState((state) => ({
+      taskList: state.taskList.concat([task])
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <h3> {this.state.userName} </h3>
+        <AddTask addNew={this.addTask} />
+        <ShowTasks tasks={this.state.taskList} />
+      </div>
+    )
+  }
+}
+
+class ShowTasks extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3> Task List: </h3>
+        <ul>
+          {this.props.tasks.map((task) => {
+            return <li> {task} </li>
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
+
+class AddTask extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      newTask: ''
+    }
+
+    this.updateNewTask = this.updateNewTask.bind(this)
+    this.handleAddNew = this.handleAddNew.bind(this)
+  }
+
+  updateNewTask(e) {
+    this.setState({
+      newTask: e.target.value
+    })
+  }
+  
+  handleAddNew() {
+    this.props.addNew(this.state.newTask)
+    this.setState({
+      newTask: ''
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.newTask}
+          onChange={this.updateNewTask}
+        />
+        <button onClick={this.handleAddNew}> Add Task </button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<TaskContainer />, document.getElementById('root'))
